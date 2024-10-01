bf689b33647256282b68b29bffab2b5469ff8ff3
Leena <leenacruz@gmail.com>
2024-09-30 18:52:52 -0700

    Project starter structure

---
+++ client/src/main.jsx
@@ -0,0 +1,44 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
+import './index.css'
+
+import App from './App.jsx'
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Error from './pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    error: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/me',
+        element: <Profile />
+      }, {
+        path: '/profiles/:username',
+        element: <Profile />
+      }, {
+        path: '/thoughts/:thoughtId',
+        element: <SingleThought />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
