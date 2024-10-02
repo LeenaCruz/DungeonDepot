2e1040819b8172bb58ea164a14179dc72b87c185
Kelsey Frahm <k.rae.heider@gmail.com>
2024-10-01 20:50:49 -0500

    Adding resoplvers and new components

--- client/src/components/ItemList/index.jsx
+++ client/src/components/ItemList/index.jsx
@@ -1,6 +1,6 @@
-const CommentList = ({ comments = [] }) => {
-  if (!comments.length) {
-    return <h3>No Comments Yet</h3>;
+const ItemList = ({ items = [] }) => {
+  if (!items.length) {
+    return <h3>No Items Yet</h3>;
   }
 
   return (
@@ -9,20 +9,20 @@ const CommentList = ({ comments = [] }) => {
         className="p-5 display-inline-block"
         style={{ borderBottom: '1px dotted #1a1a1a' }}
       >
-        Comments
+        Items
       </h3>
       <div className="flex-row my-4">
-        {comments &&
-          comments.map((comment) => (
-            <div key={comment._id} className="col-12 mb-3 pb-3">
+        {items &&
+          items.map((item) => (
+            <div key={item._id} className="col-12 mb-3 pb-3">
               <div className="p-3 bg-dark text-light">
                 <h5 className="card-header">
-                  {comment.commentAuthor} commented{' '}
+                  {item.itemAuthor} itemed{' '}
                   <span style={{ fontSize: '0.825rem' }}>
-                    on {comment.createdAt}
+                    on {item.createdAt}
                   </span>
                 </h5>
-                <p className="card-body">{comment.commentText}</p>
+                <p className="card-body">{item.itemText}</p>
               </div>
             </div>
           ))}
@@ -31,4 +31,4 @@ const CommentList = ({ comments = [] }) => {
   );
 };
 
-export default CommentList;
+export default ItemList;
