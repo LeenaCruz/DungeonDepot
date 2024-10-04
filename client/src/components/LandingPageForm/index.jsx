d9bb49f1586980da9d7bab6669c71cd9c7762c68
Leena <leenacruz@gmail.com>
2024-10-03 23:24:11 -0700

    CSS style for dashboard

--- client/src/components/LandingPageForm/index.jsx
+++ client/src/components/LandingPageForm/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import kingDice from '../../assets/kingDice.jpeg'
+import kingDice from '../../assets/kingDice.png'
 
 import { ADD_THOUGHT } from '../../utils/mutations';
 import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
