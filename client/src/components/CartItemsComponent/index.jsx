f473dfdbe6f7ac9c966f5af0d3fbafe77266a020
JA <69608172+ClassJay@users.noreply.github.com>
2024-10-07 22:49:40 -0500

    shopping cart

--- client/src/components/CartItemsComponent/index.jsx
+++ client/src/components/CartItemsComponent/index.jsx
@@ -5,7 +5,7 @@ const CartItemsComponent = () => {
     const {cart, setCart} = useCartContext();
   return (
     <div className='div-box'>
-        {cart.length === 0 ? (<div>Cart is empty</div>) : cart.map((item, index) => (<div className='item-sep'>{item.name}</div>))}
+        {cart.length === 0 ? (<div className='cart-text'>Cart is empty</div>) : cart.map((item, index) => (<div className='item-sep'>{item.name}</div>))}
     </div>
   )
 }
