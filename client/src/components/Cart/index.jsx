662f379a98df3588c940b9365922b9b1275973ac
JA <69608172+ClassJay@users.noreply.github.com>
2024-10-07 22:37:43 -0500

    Added the shopping cart

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,14 +1,18 @@
 import React, {useState} from 'react'
 // import {useOutletContext} from 'react-router-dom'
 import { useCartContext } from '../../utils/context';
+import CartItemsComponent from '../CartItemsComponent';
 
 const Cart = () => {
     const {cart, setCart} = useCartContext();
   // console.log(cartItems)
   return (
     <div className='shopping-cart'>
-      <div> {JSON.stringify(cart)} </div>
-      {/* {cartItems.length === 0 ? (<div>Cart is empty</div>) : cartItems.map((item, index) => (<div>{item.name}</div>))} */}
+      {/* <div> {JSON.stringify(cart)} </div> */}
+      <h2 className='cart-pos'>Shopping Cart
+      <CartItemsComponent />
+      </h2>
+      {/* {cart.length === 0 ? (<div>Cart is empty</div>) : cart.map((item, index) => (<div className='item-sep'>{item.name}</div>))} */}
     </div>
   )
 }
---
+++ client/src/components/CartItemsComponent/index.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import { useCartContext } from '../../utils/context';
+
+const CartItemsComponent = () => {
+    const {cart, setCart} = useCartContext();
+  return (
+    <div className='div-box'>
+        {cart.length === 0 ? (<div>Cart is empty</div>) : cart.map((item, index) => (<div className='item-sep'>{item.name}</div>))}
+    </div>
+  )
+}
+
+export default CartItemsComponent
+
+      
