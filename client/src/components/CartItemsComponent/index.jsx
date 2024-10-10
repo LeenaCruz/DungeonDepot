416e4e1f3d14fcbfffce31c97a731eb671fe2dad
JA <69608172+ClassJay@users.noreply.github.com>
2024-10-09 20:09:54 -0500

    Added clear cart button

--- client/src/components/CartItemsComponent/index.jsx
+++ client/src/components/CartItemsComponent/index.jsx
@@ -1,11 +1,16 @@
 import React, {useState} from 'react'
 import { useCartContext } from '../../utils/context';
 
+const handleClick = () => {
+console.log(Hi)
+}
+
 const CartItemsComponent = (props) => {
     const {cart, setCart} = useCartContext();
   return (
     <div className='div-box'>
         {cart.length === 0 ? (<div className='cart-text'>Empty</div>) : cart.map((item, index) => (<div className='item-sep'>{item.name}</div>))}
+        <button style={{color: '#4C061D', fontWeight: 'bold', marginTop: '15px', cursor: 'pointer'}} onClick={handleClick}>Clear Cart</button>
     </div>
   )
 }
